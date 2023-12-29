b74dba17bd328eb9a86d57c19207103c8afa21d2
MrRobaboba <50331557+MrRobaboba@users.noreply.github.com>
2023-12-29 16:32:24 +0100

    borrrar proyecto

--- assets/css-footer-home.php
+++
@@ -1,6 +0,0 @@
-<style>
-    .titulo-pregunta {
-    text-decoration: overline;
-    border: 1px #4994d5 solid;
-    }
-</style>
--- assets/footer.php
+++
@@ -1,8 +0,0 @@
-<footer>
-    <p class="titulo-secundario">Esto es un párrafo con la clase 'titulo-secundario'</p>
-    (C) Web para el máster SEO de Asdrúbal de 
-    <address> <a href="mailto:xxx@test.com">Antonio Ruiz</a> </br>
-    Jaén
-    </address>
-</footer>
-<?php echo "Página de prueba en PHP";?>
